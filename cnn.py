abdd65630045c354326975856a1a53d0632e6bbf
Nicolas <kaenzign@student.ethz.ch>
2017-10-31 16:13:35 +0100

    - Dense CNN - plot accuracies & losses improved & smoothing

--- cnn.py
+++ cnn.py
@@ -13,7 +13,7 @@ import json
 import os
 
 
-MODEL_TAG = 'dvs_36_avgPool_batchND40_L2'
+MODEL_TAG = 'dvs_36_D512_D512_L2'
 EULER = True
 TENSORBOARD = False
 CHECKPOINTS = True
@@ -22,7 +22,7 @@ BATCH_NORMALIZATION = True
 
 batch_size = 32
 num_classes = 4
-epochs = 10
+epochs = 30
 
 # input image dimensions
 img_rows, img_cols = 36, 36
@@ -37,73 +37,65 @@ processed_path += 'full_36/full/'
 hdf5_train = h5py.File(processed_path + 'train_dvs.hdf5','r')
 hdf5_test = h5py.File(processed_path + 'test_dvs.hdf5','r')
 
-# x_test = hdf5_train['images'][-10:] # TODO: load seperate hdf5 file with validation data
-# x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-#
-# y_test = hdf5_train['labels'][-10:]
-# y_test = keras.utils.to_categorical(y_test, num_classes)
+dimensions = (batch_size,img_rows,img_cols,1)
+dimensions = (batch_size,img_rows*img_cols)
+
 
 train_batches = misc.generate_batches_from_hdf5_file(hdf5_file=hdf5_train,
                                                      batch_size=batch_size,
-                                                     dimensions=(batch_size,img_rows,img_cols,1),
+                                                     dimensions=dimensions,
                                                      num_classes=num_classes,
                                                      shuffle=True)
 
 test_batches = misc.generate_batches_from_hdf5_file(hdf5_file=hdf5_test,
                                                     batch_size=batch_size,
-                                                    dimensions=(batch_size,img_rows,img_cols,1),
+                                                    dimensions=dimensions,
                                                     num_classes=num_classes,
                                                     shuffle=False)
 
 
-model = Sequential()
-model.add(Conv2D(4, kernel_size=(5, 5), input_shape=(img_rows, img_cols, 1), bias_regularizer=BIAS_REGULARIZER))
-# if BATCH_NORMALIZATION:
-#     model.add(BatchNormalization())
-model.add(Activation('relu'))
-
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(4, (5, 5), bias_regularizer=BIAS_REGULARIZER))
-# if BATCH_NORMALIZATION:
-#     model.add(BatchNormalization())
-model.add(Activation('relu'))
-
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-
-model.add(Dense(40, bias_regularizer=BIAS_REGULARIZER))
-if BATCH_NORMALIZATION:
-    model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.25))
-
-model.add(Dense(num_classes, bias_regularizer=BIAS_REGULARIZER))
-# if BATCH_NORMALIZATION:
-#     model.add(BatchNormalization())
-model.add(Activation('softmax'))
-
-# model.add(Conv2D(32, kernel_size=(3, 3),
-#                  activation='relu',
-#                  input_shape=(img_rows, img_cols, 1)))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
+# model = Sequential()
+# model.add(Conv2D(4, kernel_size=(5, 5), input_shape=(img_rows, img_cols, 1), bias_regularizer=BIAS_REGULARIZER))
+# # if BATCH_NORMALIZATION:
+# #     model.add(BatchNormalization())
+# model.add(Activation('relu'))
+#
+# #model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(AveragePooling2D(pool_size=(2, 2)))
+#
+# model.add(Conv2D(4, (5, 5), bias_regularizer=BIAS_REGULARIZER))
+# # if BATCH_NORMALIZATION:
+# #     model.add(BatchNormalization())
+# model.add(Activation('relu'))
+#
+# #model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(AveragePooling2D(pool_size=(2, 2)))
 # model.add(Flatten())
-# model.add(Dense(128, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(num_classes, activation='softmax'))
+#
+# model.add(Dense(40, bias_regularizer=BIAS_REGULARIZER))
+# if BATCH_NORMALIZATION:
+#     model.add(BatchNormalization())
+# model.add(Activation('relu'))
+# model.add(Dropout(0.25))
+#
+# model.add(Dense(num_classes, bias_regularizer=BIAS_REGULARIZER))
+# # if BATCH_NORMALIZATION:
+# #     model.add(BatchNormalization())
+# model.add(Activation('softmax'))
+
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(img_cols*img_rows,), bias_regularizer=BIAS_REGULARIZER))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', bias_regularizer=BIAS_REGULARIZER))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax', bias_regularizer=BIAS_REGULARIZER))
+
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-# for e in range(epochs):
-#     print("epoch %d" % e)
-#     for x_batch, y_batch in train_batches:
-#         model.fit(x_batch, y_batch, batch_size=32, nb_epoch=1)
 
 num_train_batches_per_epoch = int((len(hdf5_train['labels']) - 1) / batch_size)
 num_test_batches_per_epoch = int((len(hdf5_test['labels']) - 1) / batch_size)
@@ -151,30 +143,3 @@ print(history.history.keys())
 
 with open(model_dir + 'history.json', 'w') as f:
     json.dump(history.history, f)
-
-# elsewhere...
-
-# with open('./model/my_dict.json') as f:
-#     my_dict = json.load(f)
-
-# summarize history for accuracy
-# plt.plot(history.history['acc'])
-# plt.plot(history.history['val_acc'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-# # summarize history for loss
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-
-
-# score = model.evaluate(x_test, y_test, verbose=0)
-# print('Test loss:', score[0])
-# print('Test accuracy:', score[1])
--- plot_history.py
+++ plot_history.py
@@ -1,25 +1,54 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
 
+def holt_winters_second_order_ewma(x, span, beta, skip=1):
+    N = x.size
+    alpha = 2.0 / (1 + span)
+    s = np.zeros((N,))
+    b = np.zeros((N,))
+    s[:skip] = x[0:skip]
+    for i in range(skip, N):
+        s[i] = alpha * x[i] + (1 - alpha) * (s[i - 1] + b[i - 1])
+        b[i] = beta * (s[i] - s[i - 1]) + (1 - beta) * b[i - 1]
+    return s
 
-with open('./model/history36_150e_shuffle.json') as f:
+
+with open('./model/full36_B32_60E_exp_nr/history.json') as f:
     history = json.load(f)
 
 # summarize history for accuracy
+plt.figure(0)
 plt.plot(history['acc'])
 plt.plot(history['val_acc'])
+plt.plot(holt_winters_second_order_ewma(np.array(history['val_acc']), 10, 0.1, 8))
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.show()
 
 # summarize history for loss
+plt.figure(1)
 plt.plot(history['loss'])
 plt.plot(history['val_loss'])
+plt.plot(holt_winters_second_order_ewma(np.array(history['val_loss']), 10, 0.1, 8))
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
+
+
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].set_title('Accuracy')
+axarr[0].plot(history['acc'])
+axarr[0].plot(history['val_acc'])
+axarr[0].plot(holt_winters_second_order_ewma(np.array(history['val_acc']), 10, 0.1, 8))
+axarr[0].legend(['train', 'test'], loc='upper left')
+
+axarr[1].set_title('Loss')
+axarr[1].plot(history['loss'])
+axarr[1].plot(history['val_loss'])
+axarr[1].plot(holt_winters_second_order_ewma(np.array(history['val_loss']), 10, 0.1, 8))
+axarr[1].legend(['train', 'test'], loc='upper left')
 plt.show()
